e7da0603b2bdc969a1dec377d6bd47d6a9fe8a39
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2021-10-25 14:09:58 -0400

    Using stateful lists and rendering lists of data

--- payment-app/src/components/Expenses/ExpenseItem.js
+++ payment-app/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  };
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   setTitle('Updated!');
+  //   console.log(title);
+  // <button onClick={clickHandler}>Change Title</button> 
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
+      
     </Card>
   );
-}
-
+};
 export default ExpenseItem;
