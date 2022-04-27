1e7e4b8f1fc14cab5e83aaf9433fd65f7ca0eb74
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2022-04-27 18:43:45 -0400

    Reading & Dispatching from a new slice

--- redux-app/src/store/index.js
+++ redux-app/src/store/index.js
@@ -1,11 +1,11 @@
 
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -20,6 +20,23 @@ const counterSlice = createSlice({
         state.showCounter = !state.showCounter;
     },
   },
+});
+
+const initialAuthState = {
+  isAuthenticated: false
+};
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
 });
 
 // const counterReducer = (state = initialState, action) => {
@@ -56,8 +73,10 @@ const counterSlice = createSlice({
 
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
