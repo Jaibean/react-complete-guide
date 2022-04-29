51cc11ba1bb4140ce0d2a4bcde5a5fe2dbc3f795
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2022-04-29 15:12:56 -0400

    Setting up pages with routes & using NavLinks

--- react-router-spa/src/App.js
+++ react-router-spa/src/App.js
@@ -1,9 +1,30 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domaim.com/products => Products Component
+//our-domain.com/product-detail/a-book
---
+++ react-router-spa/src/pages/Welcome.js
@@ -0,0 +1,7 @@
+
+
+const Welcome = () => {
+    return  <h1>The Welcome page</h1>
+}
+
+export default Welcome;
