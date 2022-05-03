93136da83d0836f505be78b9f11b1937346ea9d8
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2022-05-03 10:13:38 -0400

    Writing more flexible routing code

--- redux-quotes-app/src/pages/QuoteDetails.js
+++ redux-quotes-app/src/pages/QuoteDetails.js
@@ -1,32 +1,35 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
-import Comments from '../components/comments/Comments';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import Comments from "../components/comments/Comments";
 
 const DUMMY_QUOTES = [
-    { id: "q1", author: "Jaimie", text: "Learning React is fun!" },
-    { id: "q2", author: "Jaimz", text: "Learning React is hard!" },
-  ];
-  
+  { id: "q1", author: "Jaimie", text: "Learning React is fun!" },
+  { id: "q2", author: "Jaimz", text: "Learning React is hard!" },
+];
+
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-      return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}`}></Route>
-      <div className='centered'>
-          <Link className='btn--flat' to={`quotes/${params.quoteId}/comments`}>Load Comments</Link>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path}></Route>
+      <div className="centered">
+        <Link className="btn--flat" to={`${match.url}/comments`}>
+          Load Comments
+        </Link>
       </div>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
-          <Comments />
+      <Route path={`${match.path}/comments`}>
+        <Comments />
       </Route>
     </Fragment>
   );
