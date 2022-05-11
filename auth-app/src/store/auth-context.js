cbc2facbb97558b0b802b84fe8f850ddf750e37f
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2022-05-11 15:34:21 -0400

    setting up context to manage auth

---
+++ auth-app/src/store/auth-context.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token; //converts to true or false boolean value
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHam,
+  };
+
+  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
